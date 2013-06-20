fe0b8a24a79c469caab0256fb87b16ab55733d05
Josh Rule <rsj28@georgetown.edu>
2013-06-20 10:39:22 +0700

    ignore log file, parameterize file names in hmaxOCV

--- bin/hmaxOCV.m
+++ bin/hmaxOCV.m
@@ -1,4 +1,4 @@
-function hmaxOCV(imgList,patchFile,maxSize,nProcs)
+function hmaxOCV(imgList,patchFile,hmaxHome,maxSize,nProcs)
 % hmaxOCV(imgList,patchFile,maxSize,nProcs)
 %
 % generate a set of HMAX-OCV activations
@@ -9,11 +9,11 @@ function hmaxOCV(imgList,patchFile,maxSize,nProcs)
 %
 % note: requires 'cp','find','xargs','mkdir', and 'echo'
 
-    if (nargin < 4) [~,sysResult] = system('nproc'); nProcs = str2num(sysResult); end;
+    if (nargin < 5) [~,result] = system('nproc'); nProcs = str2num(result); end;
 
     % make nProcs directories
-    system('rm -rf /home/joshrule/maxlab/opencv/images/*');
-    dirNames = arrayfun(@(x) ['/home/joshrule/maxlab/opencv/images/' num2str(x) '/'],...
+    system(['rm -rf ' hmaxHome 'images/*']);
+    dirNames = arrayfun(@(x) [hmaxHome 'images/' num2str(x) '/'],...
                         0:nProcs-1,'UniformOutput',0);
     cellfun(@(x) system(['mkdir ' x]),dirNames,'UniformOutput',0);
     dirString = [sprintf('%s\\n',dirNames{1:end-1}), dirNames{end}];
@@ -22,24 +22,23 @@ function hmaxOCV(imgList,patchFile,maxSize,nProcs)
     for iImg = 1:length(imgList)
         imgDirs(iImg) = mod(iImg,nProcs);
         [fa,fb,fc] = fileparts(imgList{iImg});
-        newImgFile{iImg} = ['/home/joshrule/maxlab/opencv/images/' ...
-                      num2str(imgDirs(iImg)) '/' fb lower(fc)];
+        newImgFile{iImg} = [hmaxHome 'images/' num2str(imgDirs(iImg)) '/' ...
+          fb lower(fc)];
         while ~exist(newImgFile{iImg},'file')
             imwrite(uint8(resizeImage(double(imread(imgList{iImg})), ...
-                                      maxSize)), newImgFile{iImg}, 'Quality',100);
+              maxSize)), newImgFile{iImg}, 'Quality',100);
         end
     end
 
     % run the simulation
     [pa,pb,pc] = fileparts(patchFile);
-    system(['echo -e "' dirString '" | xargs -I % -n 1 -P 0'...
-            ' ~/maxlab/opencv/bin/HMAX_FE -id % -pd ' pa '/ -pf ' pb pc...
-            ' > ~/maxlab/opencv/hmaxOCV.log']);
+    system(['echo -e "' dirString '" | xargs -I % -n 1 -P 0 ' hmaxHome ...
+      'bin/HMAX_FE -id % -pd ' pa '/ -pf ' pb pc ' > ' hmaxHome 'hmaxOCV.log']);
 
     % copy the results back!
     for iImg = 1:length(imgList)
         [fa,fb,fc] = fileparts(imgList{iImg});
-        system(['cp /home/joshrule/maxlab/opencv/images/'...
-                num2str(imgDirs(iImg)) '/' fb '.xml ' fa]);
+        system(['cp ' hmaxHome 'images/' num2str(imgDirs(iImg)) ...
+          '/' fb '.xml ' fa]);
     end
 end
